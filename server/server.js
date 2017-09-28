d0fe368fca7510eee8ce45a2fad5fc11387d0de2
Jcroxford <takeflightsc@gmail.com>
2017-09-27 19:46:21 -0600

    added basic userModel and rest route to create user
     this rest routs and user model are just a template to work from. Everything about this commit is really a template. Any tests, models, or routes can be changed but will give us a base to work from

--- server/server.js
+++ server/server.js
@@ -8,27 +8,32 @@ const app = express()
 
 // load custom .env variables
 require('./config/config')
+const isDevEnv = process.env.NODE_ENV === 'development'
 
 // load db config
 const mongoose = require('./db/mongoose')
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
-if(process.env.NODE_ENV === 'development') {
+
+if(isDevEnv) {
   db.once('open', () => console.log('db connected successfully'))
 }
 
 // load middlewares
 app.use(helmet())
 app.use(cors())
-app.use(logger('dev'))
 app.use(bodyParser.json())
+
+if(isDevEnv) {
+  app.use(logger('dev'))
+}
 
 // attach routes to app
 require('./routes')(app)
 
 const port = process.env.PORT
 app.listen(port, () => {
-  if(process.env.NODE_ENV === 'development') {
+  if(isDevEnv) {
     console.log(`server listening on http://localhost:${port}`)
   }
 })
